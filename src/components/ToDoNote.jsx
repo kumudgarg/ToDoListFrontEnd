4c648cbefbb073276cdd1967d52cf8b87da52e75
Ayush Saraf <ayush.saraf47@gmail.com>
2020-02-19 15:41:49 +0530

    Refactored : to handle scenario where update is being clicked and called without changing description

--- src/components/ToDoNote.jsx
+++ src/components/ToDoNote.jsx
@@ -6,32 +6,43 @@ class ToDoNote extends Component {
         this.state = {
             id: this.props.toDo.id,
             completed: this.props.toDo.completed,
-            description: this.props.toDo.description
+            description: this.props.toDo.description,
+            isDescriptionChanged: false
         }
     }
 
     handleCheckBoxChange = () => {
-        this.setState({completed: !this.state.completed})
-        this.update()
-    }
-
-    update = () => {
-        this.props.onUpdate(this.state)
+        this.setState({
+            completed: !this.state.completed,
+            isDescriptionChanged: true
+        }, this.update)
     }
 
     handleDescriptionChange = event => {
-        this.setState({description : event.target.value})
+        this.setState({
+            description: event.target.value,
+            isDescriptionChanged: true
+        })
+    }
+
+    update = () => {
+        if (this.state.isDescriptionChanged) {
+            this.setState({isDescriptionChanged: false})
+            this.props.onUpdate(this.state)
+        }
     }
 
     render() {
         let toDo = this.props.toDo;
         return (
             <React-Fragment>
-                <input className={"NoteLayout"} id={"isComplete"} type={"checkbox"} defaultChecked={this.state.completed} onChange={this.handleCheckBoxChange}/>
+                <input className={"NoteLayout"} id={"isComplete"} type={"checkbox"}
+                       defaultChecked={this.state.completed} onChange={this.handleCheckBoxChange}/>
                 <input className={"NoteCompLayout"} id={"description"} type={"text"} defaultValue={toDo.description}
-                       onChange={(e)=>this.handleDescriptionChange(e)}/>
+                       onChange={(e) => this.handleDescriptionChange(e)}/>
                 <button className={"NoteCompLayout"} id={"update"} onClick={() => this.update()}>update</button>
-                <button className={"NoteCompLayout"} id={"delete"} onClick={() => this.props.onDelete(toDo.id)}>delete</button>
+                <button className={"NoteCompLayout"} id={"delete"} onClick={() => this.props.onDelete(toDo.id)}>delete
+                </button>
             </React-Fragment>
         );
     }
